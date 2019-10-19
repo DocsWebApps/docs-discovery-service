cc9a9cf12257a18f8befe18dd188cddecc7516a7
Dave Collier <lardelbow@gmail.com>
2019-10-19 13:09:27 +0100

    Initial commit

---
+++ src/test/java/com/docswebapps/docsdiscoveryservice/DocsDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docswebapps.docsdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
